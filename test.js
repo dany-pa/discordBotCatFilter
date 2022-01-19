d307c6df52698949b603fedffb0689d2fb6185b1
Patskin Daniil <patskin.daniil@ihl.ru>
2022-01-19 22:54:27 +0300

    q

--- test.js
+++ test.js
@@ -4,19 +4,19 @@
 // const fs = require('fs');
 // const axios = require('axios');
 // const nrc = require('node-run-cmd');
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 
-exec("curl -s https://catfilterdiscord.s3.eu-west-2.amazonaws.com/source_3.png | rembg > output2.png", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+// exec("curl -s https://catfilterdiscord.s3.eu-west-2.amazonaws.com/source_3.png | rembg > output2.png", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
 
 
 
@@ -69,3 +69,6 @@ exec("curl -s https://catfilterdiscord.s3.eu-west-2.amazonaws.com/source_3.png |
 
 
 // s3.uploadFile(uploadParams).promise()
+
+
+console.log('123')
